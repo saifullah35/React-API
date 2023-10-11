dc7325235024af56776bb8b09428e44969d03045
Saif <saif@Arifs-MBP-4.lan>
2023-10-11 14:24:27 -0400

    building list of images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,5 +1,12 @@
+import ImageShow from './ImageShow';
+
 function ImageList ({ images }) {
-    return <div>ImageList: {images.length}</div>;
+    const renderedImages = images.mao((image) => {
+        return <ImageShow image={image} />;
+
+    });
+
+    return <div>{renderedImages} </div>;
 }
 
 export default ImageList;
